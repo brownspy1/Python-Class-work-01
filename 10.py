ed8be1a20cbba41615b8cd5671f8f7a0422b0468
M.Mahadi <motivation1424@gmail.com>
2024-01-23 14:42:57 +0600

    Update 10.py

---
+++ 10.py
@@ -0,0 +1,18 @@
+#Largast and semilast elimat in arrye
+set = [10, 324, 45, 90, 9808]
+print(max(set))
+
+#Largast and semilast elimat in arrye 02
+
+def largest(arr,n):
+
+    max = arr[0]
+    for i in range(1,n):
+        if arr[i]>max:
+            max = arr[i]
+    return max
+# for run program input exmp 10 20 50 40 20 30 80 45
+set = list(map(int,input().split()))
+n = len(set)
+num =largest(set,n)
+print(num)
