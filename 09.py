9537cb5f0a5080a08e1780772205777033e33821
M.Mahadi <motivation1424@gmail.com>
2024-01-23 13:50:37 +0600

    add

---
+++ 09.py
@@ -0,0 +1,7 @@
+nadim_gf_list = ['Sadia', 'tisha', 'Maliha', 'Sriti']
+print('List of nadim gf')
+print(nadim_gf_list)
+while True:
+    remov = input('Want to breakup with any gf of Nadeem:')
+    nadim_gf_list.remove(remov)
+    print(nadim_gf_list)
