8a23f980ca912da38319826a1ecd8b4121517b53
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-01-26 11:28:01 +0600

    Update 19.py

--- 19.py
+++ 19.py
@@ -2,6 +2,6 @@
 #Finding ASCII carectar value
 def find_ASCII(caractar):
     caractar = caractar
-    print(f'Your caracter is  {caractar} \nand your ASCII code is: {ord(caractar)}')
+    print(f'Your caracter is  {caractar} \n and your ASCII code is: {ord(caractar)}')
 ascii = input('Enter:')
 find_ASCII(ascii)
